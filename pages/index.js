7e5dea883bcaa40212bc3c883a3e4b55e7bce532
Lucas Santiago Alves <lucas1906santiago@gmail.com>
2023-08-16 17:45:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Seja Bem vindo, Luquinhas 🥸🥸🥸</h1>
+  return <h1>Seja Bem vindo, Luquinhas 🥸🥸🥸</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
